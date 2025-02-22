56fa6466338a27810c8698aaa1145da503fa53b0
Stepan Kravtsov <Stepankravtsov0402@gmail.com>
2025-02-21 23:52:17 -0500

    Initial Commit

---
+++ HacklyticsT2M/HacklyticsT2MApp.swift
@@ -0,0 +1,17 @@
+//
+//  HacklyticsT2MApp.swift
+//  HacklyticsT2M
+//
+//  Created by Степан Кравцов on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct HacklyticsT2MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
